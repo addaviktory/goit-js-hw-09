ac8438b1f6db7d55c2f1e1103dafbe17686f3dc3
addaviktory <112877379+addaviktory@users.noreply.github.com>
2023-02-25 23:01:06 +0200

    2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,86 +1,92 @@
+
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
-  inputDateEl: document.querySelector('#datetime-picker'),
-  btnEl: document.querySelector('[data-start]'),
-  daysEl: document.querySelector('[data-days]'),
-  hoursEl: document.querySelector('[data-hours]'),
-  minEl: document.querySelector('[data-minutes]'),
-  secEl: document.querySelector('[data-seconds]'),
-};
-
-setDisabledAttribute(refs.btnEl);
-refs.btnEl.addEventListener('click', start);
-
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose() {
-    const isChosenDate = dates();
-    if (isChosenDate < 0) {
-      Notify.failure('Please choose a date in the future');
-      return;
-    }
-    Notify.success('Valid date chosen, press "Start"');
-    removeAttributeDisabled(refs.btnEl);
-  },
-};
-////
-flatpickr(refs.inputDateEl, options);
-
-function start() {
-  setDisabledAttribute(refs.btnEl);
-  setInterval(() => {
-    const resultDate = dates();
-
-    if (resultDate < 0) {
-      return;
-    }
-    const timeComponents = convertMs(resultDate);
-    return updateClockFace(timeComponents);
-  }, 1000);
-}
-
-function dates() {
-  const selectedDate = new Date(refs.inputDateEl.value).getTime();
-  const currentTime = new Date().getTime();
-  const deltaTime = selectedDate - currentTime;
-  return deltaTime;
-}
+    inputDate: document.querySelector('#datetime-picker'),
+    button: document.querySelector('[data-start]'),
+    days: document.querySelector('[data-days]'),
+    hours: document.querySelector('[data-hours]'),
+    min: document.querySelector('[data-minutes]'),
+    sec: document.querySelector('[data-seconds]'),
+  };
 
 function convertMs(ms) {
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+  }
 
-  const days = pad(Math.floor(ms / day));
-  const hours = pad(Math.floor((ms % day) / hour));
-  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
-  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+  setDisabledAtr(refs.button)
+  refs.button.addEventListener('click', start)
 
-  return { days, hours, minutes, seconds };
+  const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose() {
+      const isShowDate = dates();
+      if (isShowDate < 0) {
+        Notify.failure('Please choose a date in the future');
+        return;
+      }
+      Notify.success('Valid date chosen, press "Start"');
+      removeAttributeDisabled(refs.btnEl);
+    },
+  };
+  flatpickr(refs.inputDate, options)
+
+  function start(){
+    setDisabledAtr(refs.button)
+    setInterval(() =>{
+      const  res = dates();
+    if(res < 0){
+    return
 }
+const componentTime = convertMs(res)
+return updateClockFace(timeComponents)
+    }, 1000)
+  }
 
-function pad(value) {
-  return String(value).padStart(2, '0');
-}
+function dates() {
+    const selectedDate = new Date(refs.inputDate.value).getTime();
+    const currentTime = new Date().getTime();
+    const deltaTime = selectedDate - currentTime;
+    return deltaTime;
+  }
 
-function updateClockFace({ days, hours, minutes, seconds }) {
-  refs.daysEl.textContent = days;
-  refs.hoursEl.textContent = hours;
-  refs.minEl.textContent = minutes;
-  refs.secEl.textContent = seconds;
-}
 
-function setDisabledAttribute(btn) {
-  btn.setAttribute('disabled', 'true');
-}
 
-function removeAttributeDisabled(btn) {
-  btn.removeAttribute('disabled');
-}
+  function pad(value) {
+    return String(value).padStart(2, '0');
+  }
+  
+  function updateClockFace({ days, hours, minutes, seconds }) {
+    refs.days.textContent = days;
+    refs.hours.textContent = hours;
+    refs.min.textContent = minutes;
+    refs.sec.textContent = seconds;
+  }
+  
+  function setDisabledAtr(btn) {
+    btn.setAttribute('disabled', 'true');
+  }
+  
+  function removeAttributeDisabled(btn) {
+    btn.removeAttribute('disabled');
+  }
