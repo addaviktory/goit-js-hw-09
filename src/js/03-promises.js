1e8553e9bf475e8ac2b9b3ffa87cf7e258487141
addaviktory <112877379+addaviktory@users.noreply.github.com>
2023-02-27 17:46:39 +0200

    HW done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('form');
+form.addEventListener('submit', handleSubmit);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const amount = parseInt(event.target.elements.amount.value);
+  const delay = parseInt(event.target.elements.delay.value);
+  const step = parseInt(event.target.elements.step.value);
+  let position = 1;
+  for (let i = 0; i < amount; i += 1) {
+        const position = i;
+        const promiseDelay = delay + i * step;
+    
+        createPromise(position, promiseDelay)
+          .then(({ position, delay }) => {
+            Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+          })
+          .catch(({ position, delay }) => {
+            Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+          });
+      }
 }
